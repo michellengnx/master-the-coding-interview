c5d5589d08f3abe5735e3cc86c9fae6d4585bd99
michellengnx <michellenguyen1608@gmail.com>
2022-06-10 20:32:59 +0700

    added challenge #2

--- section-3/main.py
+++ section-3/main.py
@@ -1,4 +1,6 @@
-# Section 3 - Big O Calculation - Challenge 1
+# Section 3 - Big O Calculation
+
+# Challenge 1
 # The below funChallenge function has O(n) - linear time complexity
 
 def anotherFunction():
@@ -11,6 +13,28 @@ def funChallenge(input):
         stranger = True
         a=a+1
     return a
+
+# Challenge 2
+# The below funChallenge function has O(n) - linear time complexity
+
+def anotherFunChallenge(input):
+    # 3 -> the operation is INDEPENDENT of the input
+    a = 5
+    b = 10
+    c = 50
+    # 3n
+    for i in input:
+        x = i + 1
+        y = i + 2
+        z = i + 3
+    # 2n
+    for j in input:
+        p = j * 2
+        q = j * 2
+    # 1
+    whoAmI = "I don't know"
+
+
 if __name__ == '__main__':
     funChallenge('Michelle')
 
